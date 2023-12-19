c8c71377a6c4ebe13f1948a74831c2124078da93
Koleduke <Ctvm1234>
2023-12-19 14:39:24 +0300

    sdf

--- src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
@@ -5,5 +5,14 @@ import java.util.List;
 
 public class ArrayListCreator {
     public ArrayList<String> createArrayList(List<String> sourceList) {
+        if(sourceList.size()<3){return new ArrayList<>();}
+        ArrayList<String> res = new ArrayList<>();
+        for (int i =0; sourceList.size()>i;i++){
+            if((i)%3==2){continue;}
+            res.add(sourceList.get(2+((i)/3)*3));
+            if(sourceList.size()<6&&i==1){break;}
+        }
+        return res;
     }
 }
+
--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -5,5 +5,19 @@ import java.util.List;
 
 public class LinkedListCreator {
     public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
+        LinkedList<Integer> res = new LinkedList<>();
+        for (Integer integer : sourceList) {
+            if (integer % 2 != 1) {
+                continue;
+            }
+            res.addFirst(integer);
+        }
+        for (Integer integer : sourceList) {
+            if (integer % 2 == 1) {
+                continue;
+            }
+            res.addLast(integer);
+        }
+        return res;
     }
 }
