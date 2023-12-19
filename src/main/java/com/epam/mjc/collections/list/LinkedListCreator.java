241e4f77b836d5b302135b8804b030db82efb4c5
Koleduke <Ctvm1234>
2023-12-19 20:02:56 +0300

    new1

--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -1,9 +1,24 @@
 package com.epam.mjc.collections.list;
 
+
 import java.util.LinkedList;
-import java.util.List;
+        import java.util.List;
 
 public class LinkedListCreator {
     public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
+        LinkedList<Integer> res = new LinkedList<>();
+        for (Integer integer : sourceList) {
+            if (integer % 2 != 1) {
+                continue;
+            }
+            res.addFirst(integer);
+        }
+        for (Integer integer : sourceList) {
+            if (integer % 2 == 1) {
+                continue;
+            }
+            res.addLast(integer);
+        }
+        return res;
     }
 }
--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -1,15 +1,38 @@
 package com.epam.mjc.collections.list;
 
-import java.util.Comparator;
-import java.util.List;
+
+
+import java.util.*;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        List <String> res = new ArrayList<>(sourceList);
+        Comparator<String> df = new ListComparator();
+
+        if(sourceList.isEmpty()){return;}
+        System.out.println(System.getProperty("java.version"));
+        for (int i =0; sourceList.size()>i;i++)
+        {
+            int min = Integer.parseInt(res.get(0));
+
+            for (String re : res) {
+                min = df.compare(String.valueOf(min), re);
+                sourceList.set(i, String.valueOf(min));
+            }
+
+            res.remove(String.valueOf(min));
+        }
+
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        int aC = Integer.parseInt(a);
+        int bC = Integer.parseInt(b);
+        if(aC*aC*5+3<bC*bC*5+3){return Integer.parseInt(a);}
+
+        return Integer.parseInt(b);
     }
 }
