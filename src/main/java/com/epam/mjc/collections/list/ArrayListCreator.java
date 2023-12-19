98cc3b34c10daa95674eb0d4643d1f005dced509
Koleduke <Ctvm1234>
2023-12-19 19:15:50 +0300

    new

--- src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
@@ -8,7 +8,8 @@ public class ArrayListCreator {
         if(sourceList.size()<3){return new ArrayList<>();}
         ArrayList<String> res = new ArrayList<>();
         for (int i =0; sourceList.size()>i;i++){
-            if((i)%3==2){continue;}
+            if((i)%3==2){continue;
+            }
             res.add(sourceList.get(2+((i)/3)*3));
             if(sourceList.size()<6&&i==1){break;}
                }
