c76b737fb6d8fd00611debca0cfd280b0f036385
Koleduke <ydel2014@gmail.com>
2023-12-19 11:27:12 +0300

    done

--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -1,9 +1,20 @@
 package com.epam.mjc.collections.list;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 public class LinkedListCreator {
     public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
+        LinkedList<Integer> res = new LinkedList<>();
+        for (int i =0; sourceList.size()>i;i++){
+            if((sourceList.get(i))%2!=1){continue;}
+            res.addFirst(sourceList.get(i));
+        }
+        for (int i =0; sourceList.size()>i;i++){
+            if((sourceList.get(i))%2==1){continue;}
+            res.addLast(sourceList.get(i));
+        }
+        return res;
     }
 }
--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -1,15 +1,36 @@
 package com.epam.mjc.collections.list;
 
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        List <String> res = new ArrayList<>(sourceList);
+        Comparator df = new ListComparator();
+
+        if(sourceList.isEmpty()){return;}
+
+        for (int i =0; sourceList.size()>i;i++)
+        {
+            int min = Integer.parseInt(res.get(0));
+
+                for (int j =0; res.size()>j;j++){
+                    min = df.compare(String.valueOf(min),res.get(j));
+                    sourceList.set(i, String.valueOf(min));
+                }
+
+                res.remove(res.indexOf(String.valueOf(min)));
+        }
+
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        int aC = Integer.parseInt(a);
+        int bC = Integer.parseInt(b);
+        if(aC*aC*5+3<bC*bC*5+3){return Integer.parseInt(a);}
+
+        return Integer.parseInt(b);
     }
 }
